dbe25ce1bd7403e3593536688d35c93c2545b5b9
哭哭肥宅 <81136993+cjlhypoxia@users.noreply.github.com>
2024-03-04 12:58:10 +0800

    connect mongodb

--- Events/ready.js
+++ Events/ready.js
@@ -1,9 +1,21 @@
-const { Events } = require('discord.js');
-
+const { Events, ActivityType } = require('discord.js');
+const mongoose = require('mongoose');
+const mongodb = process.env.mongodb;
+mongoose.set('strictQuery', false);
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
-	execute(client) {
+	async execute(client) {
+		await mongoose.connect(mongodb)
+			.then(() => console.log('Connected!'));
+		if (mongoose.connect) {
+			console.log('MongoDB連接成功');
+		}
+		// if (Levels.setURL(mongodb)) {
+		//	console.log('MongoDB設定成功');
+		// }
+		client.user.setPresence({ status: 'dnd' });
+		client.user.setActivity('試試 /google', { type: ActivityType.Playing });
 		console.log(`Ready! ${client.user.tag} 上線`);
 	},
 };

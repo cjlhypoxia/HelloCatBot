1a42bef5a6cf84565a42705c30862aad5ee93bed
哭哭肥宅 <81136993+cjlhypoxia@users.noreply.github.com>
2024-03-06 11:48:21 +0800

    update gif avatar

--- Events/Clients/ready.js
+++ Events/Clients/ready.js
@@ -1,6 +1,9 @@
 const { Events, ActivityType } = require('discord.js');
 const mongoose = require('mongoose');
+const fs = require('fs');
+const path = require('path');
 const mongodb = process.env.mongodb;
+
 mongoose.set('strictQuery', false);
 module.exports = {
 	name: Events.ClientReady,
@@ -11,5 +14,9 @@ module.exports = {
 		client.user.setPresence({ status: 'dnd' });
 		client.user.setActivity('試試 /gemini', { type: ActivityType.Playing });
 		console.log(`Ready! ${client.user.tag} 上線`);
+		const pathfile = path.resolve('./Data/Avatar/', 'giphy.gif');
+		const avatargif = fs.readFileSync(pathfile);
+		const avatar = Buffer.from(avatargif);
+		await client.user.setAvatar(avatar);
 	},
 };

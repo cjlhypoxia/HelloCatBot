a21dd43cea553bbace76f0b078dc7d7b58bab328
哭哭肥宅 <81136993+cjlhypoxia@users.noreply.github.com>
2024-03-13 14:40:35 +0800

    add style options

--- Commands/General/image.js
+++ Commands/General/image.js
@@ -7,16 +7,39 @@ const engineId = 'stable-diffusion-xl-1024-v1-0';
 const url = `https://api.stability.ai/v1/generation/${engineId}/text-to-image`;
 const apiKey = process.env.STABILITY_API_KEY;
 module.exports = {
-	cooldown: 5,
+	cooldown: 15,
 	data: new SlashCommandBuilder()
 		.setName('image')
 		.setDescription('用文字生成圖片（Stable Diffusion XL 1.0）')
 		.addStringOption(option =>
-			option.setName('文字').setDescription('輸入你想像的一段文字').setRequired(true)),
+			option.setName('文字').setDescription('輸入你想像的一段文字').setRequired(true))
+		.addStringOption(option =>
+			option.setName('風格').setDescription('選擇想要的風格（不選則由AI依語意創作）').setRequired(false)
+				.addChoices(
+					{ name: '3D模型（3d-model）', value: '3d-model' },
+					{ name: '類比底片（analog-film）', value: 'analog-film' },
+					{ name: '卡通風格（anime）', value: 'anime' },
+					{ name: '電影風格（cinematic）', value: 'cinematic' },
+					{ name: '漫畫風格（comic-book）', value: 'comic-book' },
+					{ name: '數位藝術（digital-art）', value: 'digital-art' },
+					{ name: '增強風格（enhance）', value: 'enhance' },
+					{ name: '幻想藝術（fantasy-art）', value: 'fantasy-art' },
+					{ name: '等距藝術（isometric）', value: 'isometric' },
+					{ name: '線條藝術（line-art）', value: 'line-art' },
+					{ name: '低多邊形（low-poly）', value: 'low-poly' },
+					{ name: '黏土塑形（modeling-compound）', value: 'modeling-compound' },
+					{ name: '賽博龐克（neon-punk）', value: 'neon-punk' },
+					{ name: '摺紙藝術（origami）', value: 'origami' },
+					{ name: '攝影風格（photographic）', value: 'photographic' },
+					{ name: '像素藝術（pixel-art）', value: 'pixel-art' },
+					{ name: '平鋪紋理（tile-texture）', value: 'tile-texture' },
+				),
+		),
 	async execute(interaction) {
 		await interaction.deferReply();
 		const prompt = interaction.options.getString('文字');
-		const inline = inlineCode('Prompt：' + prompt);
+		const style = interaction.options.getString('風格');
+		const inline = inlineCode('Prompt：' + prompt + `\nStyle：${style}`);
 		const translateprompt = await translate(prompt, { to: 'en' });
 		if (!apiKey) {
 			return interaction.editReply('發生錯誤，請稍後再試');
@@ -39,6 +62,7 @@ module.exports = {
 				width: 1024,
 				steps: 20,
 				samples: 1,
+				style_preset: style,
 			}),
 		},
 		);
